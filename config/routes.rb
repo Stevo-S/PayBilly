a1ab9f3abeec6912eecd0060979187828fa2431e
Steve Savio <savio@kwara.com>
2020-08-12 17:50:30 +0300

    Add devise for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   devise_for :users
+  # devise_for :users
+  # devise_for :admin_users, ActiveAdmin::Devise.config
+  # ActiveAdmin.routes(self)
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :c2b_transactions, only: [:index, :create, :show]
   post '/c2b_transactions/validate', as: 'validate_c2b_transaction'
